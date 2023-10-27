98f20aa68a00fd83e003d8fb753bc6267444141f
Kgarmon99 <119528390+Kgarmon99@users.noreply.github.com>
2023-10-27 14:38:06 +0000

    1st test

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 const registrationContainer = document.getElementById('registration-container');
 const avatarContainer = document.getElementById('avatar-container');
 const financialContainer = document.getElementById('financial-container');
-const goalSettingContainer = document.getElementById('goal-container');
+const goalSettingContainer = document.getElementById('goal-container'); // Updated ID
 const dashboardContainer = document.getElementById('dashboard-container');
 
 // Get references to the "Next" buttons
 const registrationNextBtn = document.getElementById('registration-next');
 const avatarNextBtn = document.getElementById('avatar-next');
 const financialNextBtn = document.getElementById('financial-next');
-const goalSettingNextBtn = document.getElementById('add-goal');
+const goalSettingNextBtn = document.getElementById('goal-setting-next'); // Updated ID
 
 // Handle "Next" button clicks
 registrationNextBtn.addEventListener('click', function() {
@@ -34,51 +34,39 @@ goalSettingNextBtn.addEventListener('click', function() {
 
 // Functions and interactions for the goal-setting section
 const goalForm = document.getElementById('goal-form');
-const goalDescriptionInput = document.getElementById('goal-description');
-const goalAmountInput = document.getElementById('goal-amount');
-const goalList = document.getElementById('goal-list');
-
 goalForm.addEventListener('submit', function(event) {
     event.preventDefault();
-
-    const goalDescription = goalDescriptionInput.value;
-    const goalAmount = goalAmountInput.value;
+    // Process the goal-setting data here
+    // You can add code to send this data to your server or perform other actions
+    const goalDescription = document.getElementById('goal-description').value;
+    const goalAmount = document.getElementById('goal-amount').value;
 
     // You can add your own logic to handle goal data (e.g., sending to a server)
     console.log('Goal Description:', goalDescription);
     console.log('Goal Amount:', goalAmount);
 
-    // Example: Display added goals in a list
-    const goalItem = document.createElement('div');
-    goalItem.textContent = `Goal: ${goalDescription}, Amount: $${goalAmount}`;
-    goalList.appendChild(goalItem);
-
-    // Clear form fields after submission
+    // Example: Clear form fields after submission
     goalForm.reset();
 });
 
 // Additional JavaScript code for the dashboard or other sections
 // Example: Handling a button click in the dashboard
 const trackIncomeButton = document.getElementById('track-income-button');
-if (trackIncomeButton) {
-    trackIncomeButton.addEventListener('click', function() {
-        // Implement what happens when the "Track Income" button is clicked
-    });
-}
+trackIncomeButton.addEventListener('click', function() {
+    // Implement what happens when the "Track Income" button is clicked
+});
 
 // Example: Adding dynamic content to the dashboard
 const dashboardContent = document.querySelector('.dashboard-content');
-if (dashboardContent) {
-    // Create a new dashboard item
-    const newDashboardItem = document.createElement('div');
-    newDashboardItem.className = 'dashboard-item';
-    newDashboardItem.innerHTML = `
-        <h2 class="dashboard-item-title">New Feature</h2>
-        <p class="dashboard-item-description">This is a new feature in MoneyBot.</p>
-        <a href="#" class="dashboard-item-button">Explore</a>
-    `;
-    // Append the new item to the dashboard
-    dashboardContent.appendChild(newDashboardItem);
-}
+// Create a new dashboard item
+const newDashboardItem = document.createElement('div');
+newDashboardItem.className = 'dashboard-item';
+newDashboardItem.innerHTML = `
+    <h2 class="dashboard-item-title">New Feature</h2>
+    <p class="dashboard-item-description">This is a new feature in MoneyBot.</p>
+    <a href="#" class="dashboard-item-button">Explore</a>
+`;
+// Append the new item to the dashboard
+dashboardContent.appendChild(newDashboardItem);
 
 // You can add more interactions and functionality for specific sections as needed
